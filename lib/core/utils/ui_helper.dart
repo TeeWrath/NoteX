97705b8b1d7bf65ca3f65c393d33a9a1426ba715
Subroto <subroto.2003@gmail.com>
2024-08-25 12:39:27 +0530

    feat: utils

---
+++ lib/core/utils/ui_helper.dart
@@ -0,0 +1,6 @@
+//! Size Config shortcuts
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+var kScreenPadding = EdgeInsets.symmetric(horizontal: 40.w);
---
+++ lib/core/utils/utils.dart
@@ -0,0 +1,3 @@
+export 'package:notex/core/utils/state_handler.dart';
+export 'package:notex/core/utils/ui_helper.dart';
+export 'package:notex/core/utils/validators.dart';
